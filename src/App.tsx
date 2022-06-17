7c60a40b7caffbc0edacc01822f29d8d6046186c
Marcelo Silva <marcelo.dev.js@hotmail.com>
2022-06-17 15:47:25 -0300

    chore: insataled react router v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
 import './Global.css'
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   	return (
-		<div>Cadastro</div>
+		<BrowserRouter>
+			<AppRoutes />
+		</BrowserRouter>
 	)
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => {
+    return(
+        <Routes>
+            <Route path="/pagina-inicial" element={<h1>Página inicial</h1>} />
+
+            <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
+        </Routes>
+    );
+}
