6233d8126df51eae8a58f8e39ba41cef389d3746
Marcelo Silva <marcelo.dev.js@hotmail.com>
2022-06-19 15:33:54 -0300

    chore: created toggle for open and close menu aside left

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import { AppThemeProvider } from './shared/contexts';
 import { MenuLateral } from './shared/components';
+import { AppDrawerProvider } from './shared/contexts';
 
 export const App = () => {
 	return (
 		<AppThemeProvider>
-			<BrowserRouter>
-				<MenuLateral>
-					<AppRoutes />
-				</MenuLateral>
-			</BrowserRouter>
+			<AppDrawerProvider>
+				<BrowserRouter>
+					<MenuLateral>
+						<AppRoutes />
+					</MenuLateral>
+				</BrowserRouter>
+			</AppDrawerProvider>
 		</AppThemeProvider>
 	);
 };
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,7 +1,8 @@
 import ImageProfile from '../../../assets/images/perfil.png';
 
-import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material";
-import { ReactNode } from "react";
+import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme, useMediaQuery } from '@mui/material';
+import { ReactNode } from 'react';
+import { useDrawerContext } from '../../contexts';
 
 interface iMenuLateral {
     children: ReactNode;
@@ -10,44 +11,49 @@ interface iMenuLateral {
 export const MenuLateral: React.FC<iMenuLateral> = ({ children }) => {
 
     const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+	const {isDrawerOpen, toggleDrawerOpen} = useDrawerContext();
 
-    return (
-        <Box>
-            <Drawer variant='permanent'>
-                <Box
-                    width={theme.spacing(28)}
-                    height='100%' display='flex'
-                    flexDirection='column' alignContent='center'
-                    justifyContent='center'
-                >
-                    <Box
-                        width='100%'
-                        height={theme.spacing(20)}
-                        display='flex'
-                        alignItems='center'
-                        justifyContent='center'
-                    >
-                        <Avatar
-                            sx={{ width: theme.spacing(9), height: theme.spacing(9) }}
-                            src={ImageProfile}
-                        />
-                    </Box>
-                    <Divider />
-                    <Box flex='1' >
-                        <List component='nav'>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <Icon>home</Icon>
-                                </ListItemIcon>
-                                <ListItemText primary="Página inicial" />
-                            </ListItemButton>
-                        </List>
-                    </Box>
-                </Box>
-            </Drawer>
-            <Box height='100vh' marginLeft={theme.spacing(28)}>
-                {children}
-            </Box>
-        </Box>
-    );
+	return (
+		<Box>
+			<Drawer open={isDrawerOpen} variant={smDown? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
+				<Box
+					width={theme.spacing(28)}
+					height='100%' display='flex'
+					flexDirection='column' alignContent='center'
+					justifyContent='center'
+				>
+					<Box
+						width='100%'
+						height={theme.spacing(20)}
+						display='flex'
+						alignItems='center'
+						justifyContent='center'
+					>
+						<Avatar
+							sx={{ width: theme.spacing(9), height: theme.spacing(9) }}
+							src={ImageProfile}
+						/>
+					</Box>
+					<Divider />
+					<Box flex='1' >
+						<List component='nav'>
+							<ListItemButton>
+								<ListItemIcon>
+									<Icon>home</Icon>
+								</ListItemIcon>
+								<ListItemText primary="Página inicial"/>
+							</ListItemButton>
+						</List>
+					</Box>
+				</Box>
+			</Drawer>
+			<Box
+                height='100vh'
+                marginLeft={ smDown? theme.spacing(0): theme.spacing(28)}
+            >
+				{children}
+			</Box>
+		</Box>
+	);
 };
