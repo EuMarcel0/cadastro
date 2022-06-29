0ac29b6eafa02cb378d54075eaf60bf909aa199a
Marcelo Silva <marcelo.dev.js@hotmail.com>
2022-06-28 22:35:26 -0300

    chore: created state with useSearchParams of react-router-dom for input of liting of people

--- src/pages/pessoas/ListagemPessoas.tsx
+++ src/pages/pessoas/ListagemPessoas.tsx
@@ -1,13 +1,30 @@
 import { Icon, Typography } from "@mui/material";
+
+import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+
 import { FerramentasListagem } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 
 export const ListagemPessoas: React.FC = () => {
+    const [searchParams, setSearchParams] = useSearchParams('Teste');
+
+    const search = useMemo(() => {
+        return searchParams.get('busca') || '';
+    },[searchParams]);
+
     return(
         <LayoutBaseDePagina
             icon={<Typography><Icon>person</Icon></Typography>} 
             title='Listagem de pessoas'
-            toolbar={<FerramentasListagem showInputSearch textButtonNew='Nova' />}
+            toolbar={<FerramentasListagem
+                        showInputSearch
+                        textButtonNew='Nova'
+                        textOfSearch={search}
+                        handleTextOfSearch={text => setSearchParams({busca: text}, {replace: true})}
+                        />
+                    }
+
         >
 
         </LayoutBaseDePagina>
